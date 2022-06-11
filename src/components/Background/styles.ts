f226467beca4f894b0c86ff3f20222aef0541703
Hélvio Filho <helviosvf@gmail.com>
2022-06-10 23:50:06 -0300

    Fix(Background): fix background image

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -1,13 +1,16 @@
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { ImageBackground } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled(ImageBackground).attrs({
   imageStyle: {
-    top: '-50%'
+    position: 'absolute',
+    top: -RFValue(165),
   },
   resizeMode: 'contain'
 })`
   flex: 1;
+  height: ${RFPercentage(100)}px;
 `;
 
 export const SafeAreaViewIOS = styled.SafeAreaView`
